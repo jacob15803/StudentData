e369bfec2f4a3edd8064c3bff7ab7fc6f4279217
Jacob <jacobmenachery15@gmail.com>
2024-07-23 13:18:18 +0530

    Added put and dlt

--- routers/task.py
+++ routers/task.py
@@ -22,3 +22,16 @@ async def create_student(student:StudentCreate ):
     return {**student.dict(),"id":student_id}
 
 
+@get_data.put("/student/{id}")
+async def update_student(id: int, student: StudentCreate):
+    query = """UPDATE student SET name = $1, age = $2, grade = $3 WHERE id = $4 RETURNING id;"""
+
+    u_student_id = await execute_query(query, student.name, student.age, student.grade, id)
+    return {**student.dict(), "id": u_student_id}
+
+@get_data.delete("/student/{id}")
+async def delete_student(id: int):
+    query = """DELETE FROM student WHERE id = $1 RETURNING id; """
+
+    d_student_id = await execute_query(query, id)
+    return {"id": d_student_id, "message": "Student deleted successfully"}
